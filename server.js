fa05c576d5c144cfd7aeb6ae4efc772f3497865b
lukejohnsen <johnsen.luke@gmail.com>
2021-12-22 12:35:04 -0700

    resetting

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require('express');
-const routes = require('./controllers');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
-const path = require('path');
-const exphbs = require('express-handlebars');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-const hbs = exphbs.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false })
-    .then(() => {
-        app.listen(PORT, () => console.log('Now listening'));
-    });
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
